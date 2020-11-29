da8e48134844cb154b612be4209548d48c458432
Faruk <felixfranko66@gmail.com>
2020-11-29 22:44:24 +0300

    forgot password and userposts added

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container } from "@material-ui/core";
+import {
+  Container,
+  Grid,
+  capitalize,
+  CircularProgress,
+} from "@material-ui/core";
+import axios from "axios";
+import MediaCard from "../components/MediaCard";
+
 const stylesFunc = makeStyles((theme) => ({
   wrapper: {
-    marginTop: "10rem",
-    height: "calc(100vh - 19.0625rem)",
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+    marginTop: "5rem",
     textAlign: "center",
   },
   avatar: {
@@ -14,11 +24,46 @@ const stylesFunc = makeStyles((theme) => ({
 }));
 
 function Main() {
+  const [userList, setUserList] = useState();
   const mainStyles = stylesFunc();
+  const { REACT_APP_API_BASE_URL, REACT_APP_API_TOKEN } = process.env;
+
+  const fetchData = async () => {
+    const response = await axios.get(`${REACT_APP_API_BASE_URL}/user`, {
+      headers: {
+        "app-id": REACT_APP_API_TOKEN,
+      },
+    });
+    setUserList(response?.data?.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <Container className={mainStyles.wrapper} maxWidth="sm">
-      MAIN PAGE
+    <Container className={mainStyles.wrapper}>
+      {!userList ? (
+        //TODO: center loading icon
+        <CircularProgress />
+      ) : (
+        <Grid container spacing={1}>
+          {userList?.map((user) => {
+            return (
+              <Grid item sm={4} xs={6} key={user?.id}>
+                <MediaCard
+                  id={user.id}
+                  userImage={user?.picture}
+                  userName={`${capitalize(user?.title)} ${user?.firstName} ${
+                    user?.lastName
+                  }`}
+                  userEmail={user?.email}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
     </Container>
   );
 }
